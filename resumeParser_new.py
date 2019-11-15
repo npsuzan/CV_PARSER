9700d0f1e356c3417f542a73ce386062b5a2db93
sujan <sujneupane@deerwalk.com>
2019-11-15 10:57:55 +0545

    final

---
+++ resumeParser_new.py
@@ -0,0 +1,53 @@
+# information-extraction.py
+
+import re
+import nltk
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('maxent_ne_chunker')
+nltk.download('words')
+
+stop = stopwords.words('english')
+
+string = """
+CA Sandeep Chandaliya,
+Age 39years
+Mumbai -12 (India)
+Email: sandeep.chandaliya@crownsec.com
+Mobile: +919619496873 Board Line: +912240690701
+"""
+
+def extract_phone_numbers(string):
+    r = re.compile(r'(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})')
+    phone_numbers = r.findall(string)
+    return [re.sub(r'\D', '', number) for number in phone_numbers]
+
+def extract_email_addresses(string):
+    r = re.compile(r'[\w\.-]+@[\w\.-]+')
+    return r.findall(string)
+
+def ie_preprocess(document):
+    document = ' '.join([i for i in document.split() if i not in stop])
+    sentences = nltk.sent_tokenize(document)
+    sentences = [nltk.word_tokenize(sent) for sent in sentences]
+    sentences = [nltk.pos_tag(sent) for sent in sentences]
+    return sentences
+
+def extract_names(document):
+    names = []
+    sentences = ie_preprocess(document)
+    for tagged_sentence in sentences:
+        for chunk in nltk.ne_chunk(tagged_sentence):
+            if type(chunk) == nltk.tree.Tree:
+                if chunk.label() == 'PERSON':
+                    names.append(' '.join([c[0] for c in chunk]))
+    return names
+
+if __name__ == '__main__':
+    numbers = extract_phone_numbers(string)
+    emails = extract_email_addresses(string)
+    names = extract_names(string)
+    print("ss")
+    print(names[0])
+    print(emails)
